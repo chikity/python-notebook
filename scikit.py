dd4d0a439eb625fc89c77a0126cba4b946015069
Chi Kit Yeung <chikit2370@gmail.com>
2017-09-30 10:51:59 +0700

    Initial upload

---
+++ scikit.py
@@ -0,0 +1,10 @@
+# from sklearn import datasets
+# iris = datasets.load_iris()
+#
+# print(iris)
+
+from sklearn import datasets
+iris = datasets.load_iris()
+digits = datasets.load_digits()
+
+print(digits.data)
